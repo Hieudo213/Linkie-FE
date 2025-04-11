d4979544a78ce623f9d8e08ea735353a9a1a7b35
TOM\hieud <sherrydo2103@gmail.com>
2025-04-11 18:01:59 +0700

    install antd tailwinds css axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "antd";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="w-full h-[100vh] flex justify-center items-center bg-pink-300 flex-col">
+            <h1 className="text-white text-[50px]">Hello Linkie!</h1>
+            <Button size={'large'} type="primary">Click here!!</Button>
+        </div>
+    );
 }
 
 export default App;
