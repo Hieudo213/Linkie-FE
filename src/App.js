4ee38a14f14f429ef452f603fb837e0484930a38
datmp18 <vutiendat1804hd@gmail.com>
2025-04-13 01:50:06 +0700

    Update -slidebar v2 -navbar v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import { Layout, theme } from 'antd';
 import './App.css';
 import NavBar from './components/NavBar';
 
 import SlideBar from './components/SlideBar';
+import { Content, Footer } from 'antd/es/layout/layout';
 
 function App() {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
     return (
         <div className='w-[100%] h-[100vh] '>
             <NavBar />
-            <SlideBar />
+            <Layout style={{ minHeight: '90vh' }}>
+                <SlideBar />
+                <Layout>
+
+                    <Content style={{ margin: '20px 16px' }}>
+
+                        <div
+                            style={{
+                                padding: 24,
+                                minHeight: 360,
+                                background: colorBgContainer,
+                                borderRadius: borderRadiusLG,
+                            }}
+                        >
+                            đây là body
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
         </div>
     );
 }
--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+
+import { Layout, Menu, theme } from 'antd';
+import { FaFlag, FaUsers } from 'react-icons/fa';
+import { BiSolidPieChartAlt2 } from 'react-icons/bi';
+import { FaChartColumn } from 'react-icons/fa6';
+const { Content, Footer, Sider } = Layout;
+
 
 function getItem(label, key, icon, children) {
     return {
@@ -17,51 +15,28 @@ function getItem(label, key, icon, children) {
         label,
     };
 }
-const items = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
+const item = [
+    getItem('Dashboard', '1', <BiSolidPieChartAlt2 />),
+    getItem('Users', 'sub1', <FaUsers />, [
         getItem('Tom', '3'),
         getItem('Bill', '4'),
         getItem('Alex', '5'),
     ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('Reports', 'sub2', <FaFlag />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Analytics', '9', <FaChartColumn />),
 ];
 const SlideBar = () => {
     const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
-    return (
-        <Layout style={{ minHeight: '90vh' }}>
-            <Sider className='bg-white' width={"16.666666%"} collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}  >
-                <div className="demo-logo-vertical" />
-                <Menu className='bg-white' defaultSelectedKeys={['1']} mode="inline" items={items} />
-            </Sider>
-            <Layout>
 
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div
-                        style={{
-                            padding: 24,
-                            minHeight: 360,
-                            background: colorBgContainer,
-                            borderRadius: borderRadiusLG,
-                        }}
-                    >
-                        Bill is a cat.
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
-                </Footer>
-            </Layout>
-        </Layout>
+    return (
+        <>
+            <Sider className='bg-white' width={"16.666666%"} collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}  >
+                <Menu className='bg-white' defaultSelectedKeys={['1']} mode="inline" items={item} />
+            </Sider>
+
+        </>
+
+
     )
 }
 
